df17e5ad023d4d2f535c1ba0e6a775e0cf5e9d08
Jacky Tang <jackytck@gmail.com>
2016-04-17 07:00:50 +0800

    * show link if book is found

--- src/components/book-list-item.js
+++ src/components/book-list-item.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
+function getLink (state, { link = '' } = {}) {
+  if (state === 'found') {
+    const chamo = link.match(/^<a href="..\/lib\/item\?id=chamo:([\d]+)/)[1]
+    return (
+      <a href={`https://webcat.hkpl.gov.hk/lib/item?id=chamo:${chamo}`} target='_blank'>{state}</a>
+    )
+  }
+  return state
+}
+
 function BookListItem ({ index, book }) {
-  const { state, title } = book
+  const { state, title, data } = book
   let resultClass = state.replace(' ', '')
   return (
     <div className={`BookItem ${resultClass}`}>
       <span className='BookItemIndex'>{`${index + 1}.`}</span>
       <span className='BookItemTitle'>{title}</span>
-      <span className={`BookItemState ${resultClass}`}>{state}</span>
+      <span className={`BookItemState ${resultClass}`}>{getLink(state, data)}</span>
     </div>
   )
 }
